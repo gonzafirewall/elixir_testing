7a65a2431c3038eb9785c3a33882cb2840a85ba4
Gonzalo Martinez <gm185176@ncr.com>
2019-02-23 21:25:32 -0300

    some conditional things

--- test.ex
+++ test.ex
@@ -30,6 +30,25 @@ defmodule M do
         IO.puts "Vote & Drive: #{(age >= 16) and (age >= 18)}"
         IO.puts "Vote or Drive: #{(age >= 16) or (age >= 18)}"
         IO.puts not true
+
+        if age >= 18 do
+            IO.puts "Can vote"
+        else
+            IO.puts "Can't vote"
+        end
+
+        unless age === 18 do
+            IO.puts "You're not 18"
+        else
+            IO.puts "You are 18"
+        end
+
+        cond do
+            age >= 18 -> IO.puts "You can vote"
+            age >= 16 -> IO.puts "You can drive"
+            age >= 14 -> IO.puts "You can wait"
+            true -> IO.puts "Default"
+        end
     end
 
     def do_compare do
